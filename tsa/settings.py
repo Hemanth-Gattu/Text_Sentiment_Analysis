06a0251da4b445a1958fb315142010b7e745a74e
Hemanth-Gattu <59017902+Hemanth-Gattu@users.noreply.github.com>
2021-07-02 17:11:18 +0530

    Update settings.py

--- tsa/settings.py
+++ tsa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'n)!5uw#9(a1#ft_s2gvzqwkxk5^aukfb&zf+6gqnf1*x!0bs#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
